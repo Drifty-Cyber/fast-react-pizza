2caccab55276a17a8a96cf1c4a686012a5900d61
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-01-26 22:54:38 +0100

    chore: delete cart items

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,15 @@
 import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { useDispatch } from 'react-redux';
-import { addItem } from '../cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     // console.log(id);
@@ -43,7 +46,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
+          {isInCart && <DeleteItem />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
